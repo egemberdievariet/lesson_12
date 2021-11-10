92eada11de7c0e20b31cfa625cf9fc0d4e237d20
Egemberdievariet <57054923+egemberdievariet@users.noreply.github.com>
2021-11-10 17:04:56 +0600

    11.10

--- main.dart
+++ main.dart
@@ -1,6 +1,9 @@
 main(List<String> args) {
   var country = new Country("KG", "Summer");
-  country.printsmth();
+  country.printSmth();
+
+  var car = new Car("Yellow", 230, "PassatCC", 8000);
+  car.printParam();
 }
 
 class Country {
@@ -9,7 +12,21 @@ class Country {
 
   Country(this.name, this.weather);
 
-  void printsmth() {
+  void printSmth() {
     print("Name of the country is: ${name}, weather is: $weather");
+  }
+}
+
+class Car {
+  String? color;
+  int? power;
+  String? name;
+  int? price;
+
+  Car(this.color, this.power, this.name, this.price);
+
+  void printParam() {
+    print(
+        "Name of the car is: $name, power is :$power, color is $color, price is $price USD");
   }
 }
